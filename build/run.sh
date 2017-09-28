a53c088978571e143c0064d06a86f9058af7039a
Oussama Barhoumi <oussama@travelcar.com>
2017-09-28 14:55:30 +0200

    Upgrade to v30 Change Payout: https://docs.adyen.com/support/frequently-asked-questions/products-and-subscriptions/how-can-i-migrate-from-refundwithdata-to-third-party-payouts
    Took 4 hours 1 minute

--- build/run.sh
+++ build/run.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-#git clone https://github.com/swagger-api/swagger-codegen
-#cd swagger-codegen
-#./run-in-docker.sh mvn package
-#cd ..
-#cp swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar swagger-codegen-cli-2.2.2.jar
-
 # Usage example: /bin/sh ./run.sh javascript adyen-api-js
 language=$1
 output=$2
@@ -20,4 +14,4 @@ if [ "$output" = "" ]; then
     echo "[INFO] No command line input provided. Set \$output to $output"
 fi
 
-java -jar swagger-codegen-cli-2.2.2.jar generate -i ../specification.yaml -l $language  -c ../client/javascript/config.json -o ../$output
+java -jar swagger-codegen-cli-2.2.3.jar generate -i ../specification.yaml -l $language  -c ../client/javascript/config.json -o ../$output
